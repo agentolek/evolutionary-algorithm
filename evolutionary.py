9049e39a3d5a4710245262203248d1256944a835
Mateusz Guliński <13matg37@gmail.com>
2024-04-23 19:19:45 +0200

    mutation dependant on avg accuracy

--- evolutionary.py
+++ evolutionary.py
@@ -30,7 +30,7 @@ def create_param_sets(gen_size):
         tmp.append(create_random_tensor(10, 64))
         tmp.append(create_random_tensor(10))
         sets.append(tmp)
-    
+
     return sets
 
 
@@ -76,7 +76,7 @@ def evaluate_all(param_list, dataloader):
 
 def softmax(x):
     """Compute softmax values for each sets of scores in x."""
-    temp = 10
+    temp = 5
     e_x = np.exp(x * temp)
     return e_x / e_x.sum()
 
@@ -86,7 +86,7 @@ def mutate_reset(array, mutation_prob):
 
     mask = np.random.randint(0,1/mutation_prob,size=array.shape).astype(bool)
     # currently generates random values in the range <-0.1, 0.1>
-    r = (np.random.rand(*array.shape) - 0.5) * 0.2
+    r = (np.random.rand(*array.shape) - 0.5) * 0.5
     array[mask] = r[mask]
     return array
 
@@ -97,20 +97,22 @@ def mutate_change(array, mutation_prob):
 
     for _ in range(mutation_num):
         mask = np.random.randint(0,int(1/mutation_prob),size=array.shape).astype(bool)
-        array[mask] += array[mask] * (np.random.rand() - 0.5) * 0.2
-    
+        array[mask] += array[mask] * (np.random.rand() - 0.5) * 0.05
+
+
     return array
 
 def mutate_set(params_set, mutation_probs):
-    mutation_factor = 2
+    reset_factor = 1
+    change_factor = 5
     new_set = []
     for i in range(len(params_set)):
         # reset_prob = (mutation_probs[i] / mutation_factor)*(21-epoch_counter)/20
         # change_prob = (mutation_probs[i] / mutation_factor)*(epoch_counter+1)/20
         # temp = mutate_reset(params_set[i], reset_prob)
         # new_set.append(mutate_change(temp, change_prob))
-        temp = mutate_reset(params_set[i], mutation_probs[i] * mutation_factor)
-        new_set.append(mutate_change(temp, mutation_probs[i] * mutation_factor))
+        temp = mutate_reset(params_set[i], mutation_probs[i] * reset_factor)
+        new_set.append(mutate_change(temp, mutation_probs[i] * change_factor))
 
     return new_set
 
@@ -123,7 +125,7 @@ def cross_breed(pair):
         split = np.random.randint(2, len(father)-2)
         child1.append(np.concatenate((dad[:split], mom[split:])))
         child2.append(np.concatenate((mom[:split], dad[split:])))
-    
+
     return child1, child2
 
 def choose_parent_indexes(num_of_pairs, prob_distribution):
@@ -136,13 +138,13 @@ def choose_parent_indexes(num_of_pairs, prob_distribution):
         selected_indexes = np.random.choice(np.arange(len(prob_distribution)), size=2*batch_size, p=prob_distribution, replace=False)
         for i in range(batch_size):
             pairs.append(selected_indexes[i*2: (i+1)*2])
-    
+
     return pairs
 
 
 def create_generation(params_sets, prob_distribution, gen_size, layer_mutate_prob):
-    
-    mutation_chance = 4
+
+    mutation_chance = 6
     pairs = choose_parent_indexes((gen_size // 2) + 1, prob_distribution)
     new_gen = []
 
@@ -150,7 +152,7 @@ def create_generation(params_sets, prob_distribution, gen_size, layer_mutate_pro
         # this for loop creates kids from pairs of params
         pair = [params_sets[x] for x in pair]
         new_gen += cross_breed(pair)
-    
+
     for i in range(gen_size):
         # this for loop is responsible for mutations in kids
 
@@ -160,44 +162,75 @@ def create_generation(params_sets, prob_distribution, gen_size, layer_mutate_pro
 
     return new_gen[:gen_size]
 
+def mutation_factor(ratio):
+    # if ratio == 0:
+    #     return 1
+    # y = 1/(ratio**(1/4)) - 1
+    # return y
+    if ratio == 0: return 1
+    if ratio <0.05: return 1.01
+    else: return 0.99
 
-def create_graph(values, values2):
-    plt.plot(values)
-    plt.plot(values2)
+
+def create_graph(avg, max_):
+    plt.plot([100*x for x in avg], label="avg")
+    plt.plot([100*x for x in max_], label="max")
+    plt.xlabel("Generation")
+    plt.ylabel("Accuracy")
+    plt.legend()
     plt.savefig("plot.png")
 
 
-def evolve(epochs, gen_size, dataloader):
+def evolve(epochs, gen_size, dataloader, test_dataloader):
 
     params_sets = create_param_sets(gen_size) # begin with random arrays of parameters
-    elitism_factor = 0.1
+    layer_mutate_prob = [0.001, 0.001, 0.002, 0.002, 0.003, 0.003, 0.004, 0.004]
+    ratio_of_acc = 0.25 # for this the function is 1 at the start
+    elitism_factor = 0.2
+    combined_factor = 1
     average_accuracy = []
     max_accuracy = []
     for _ in range(epochs):
-        
-        # the following lines create tables to plot for later debugging 
+
+        # the following lines create tables to plot for later debugging
         accuracies = np.array(evaluate_all(params_sets, dataloader))
         average_accuracy.append(sum(accuracies)/gen_size)
         max_accuracy.append(max(accuracies))
-        
+
+        layer_mutate_prob = [x*mutation_factor(ratio_of_acc) for x in layer_mutate_prob]
+        combined_factor *= mutation_factor(ratio_of_acc)
+        last_x_avg = sum(average_accuracy[-30:])/len(average_accuracy[-30:])
+        all_avg= sum(average_accuracy)/len(average_accuracy)
+
+        #near 0 if stable - need more mutation
+        #higher - unstable - less mutation
+        ratio_of_acc = abs(1 - last_x_avg/all_avg)
+        print(combined_factor)
+        print(ratio_of_acc)
+        print("\n")
+
+        # print(mutation_factor(ratio_of_acc))
+        # print(layer_mutate_prob[-1])
+
         prob_distribution = softmax(accuracies)
         # selected_index = np.random.choice(np.arange(gen_size), size=gen_size//2, p=prob_distribution, replace=False)
         # selected_params = [params_sets[x] for x in selected_index]
 
         temp = sorted(list(zip(prob_distribution, params_sets)), reverse=True, key=lambda x:x[0])[:int(gen_size*elitism_factor)]
         temp = list(list(zip(*temp))[1])
-        params_sets = temp + create_generation(params_sets, prob_distribution, math.ceil(gen_size*(1-elitism_factor)), layer_mutate_prob=[0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08])
-        
+        params_sets = temp + create_generation(params_sets, prob_distribution, math.ceil(gen_size*(1-elitism_factor)), layer_mutate_prob)
+
+
+
     create_graph(average_accuracy, max_accuracy)
-    temp = list(zip(evaluate_all(params_sets, dataloader), params_sets))
+    temp = list(zip(evaluate_all(params_sets, test_dataloader), params_sets))
     temp = sorted(temp, key=lambda x: x[0])
     print(temp[0][0], temp[-1][0])
     return temp[-1][1]
-    
+
 if __name__ == "__main__":
     datasets = random_split(transformed_dataset, (898, 899))
     train_dataloader = DataLoader(datasets[0], batch_size=40, shuffle=True)
     test_dataloader = DataLoader(datasets[1], batch_size=40, shuffle=True)
-    params = evolve(200, 20, train_dataloader)
+    params = evolve(2000, 20, train_dataloader, test_dataloader)
     print("Evolved accuracy: " + str(evaluate(params, test_dataloader)))
-    
